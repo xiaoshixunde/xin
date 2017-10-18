c85b85ea3aaa4512b2b25cdad88b27c5a3aad97f
Administrator <1486116063@qq.com>
2017-10-18 21:23:35 +0800

    这是第四天的课程

---
+++ app/src/main/java/com/mei/day08/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mei.day08;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this, "jownhmownn", Toast.LENGTH_SHORT).show();
+    }
+}
